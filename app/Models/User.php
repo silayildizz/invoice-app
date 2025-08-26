723e8950971d38309b32311bb6148b28dedea3eb
sila.yildiz <sila.yildiz@TRHQMLZLMD6N.local>
2025-08-26 10:20:46 +0300

    sifremi unuttum kısmına ait güncellemeler

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,8 +6,13 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Contracts\Auth\CanResetPassword as CanResetPasswordContract;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
-class User extends Authenticatable
+
+
+
+class User extends Authenticatable 
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
@@ -46,3 +51,4 @@ class User extends Authenticatable
         ];
     }
 }
+
